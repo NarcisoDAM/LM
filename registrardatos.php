e2201aa9366198d3d6e62d938ab0ade3d865f767
NarcisoDAM <2913047@alu.murciaeduca.es>
2021-11-02 22:42:07 +0100

    añadir registrardatos.php

---
+++ registrardatos.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		Bienvenido <?php echo $_GET["nombre"]; ?><br>
+		Tu apellido es: <?php echo $_GET["apellido"]; ?>
+	</body>
+</html>
---
+++ registrardatos2.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		Bienvenido <?php echo $_GET["nombre"]; ?><br>
+		Tu clave es: <?php echo $_GET["clave"]; ?>
+	</body>
+</html>
